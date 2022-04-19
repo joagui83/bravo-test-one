f55056b071e85be6f879ee1287a4a23a5272eb25
joagui83 <jjaggie83@gmail.com>
2022-04-19 10:58:42 -0700

    initial commit

---
+++ ruby-hash.rb
@@ -0,0 +1,11 @@
+# Ruby hashes => is known as a hash rocket
+learn_circus = {
+    acrobat: 'Mulan',
+    contortionist: 'Canelo',
+    clown: 'Pantera',
+    }
+p learn_circus
+
+#another method of creating a hash 
+learn_band = Hash.new
+p learn_band
